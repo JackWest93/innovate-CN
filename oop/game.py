065014176d1d2bc7c5d74d74e2873eb605c66b32
Jack.West <westjack958@gmail.com>
2022-06-13 14:54:27 +0100

    additional classes

--- oop/game.py
+++ oop/game.py
@@ -4,7 +4,7 @@ Superman=Character("Clark Kent", "Superman")
 Hulk=Character("Bruce Banner", "Hulk")
 Batman=Character("Bruce Wayne", "Batman")
 
-Superman.superpower("Super-strength, super speed, flying.")
+Superman.superpower("Super strength, super speed, flying.")
 Hulk.superpower("Super strength")
 Batman.superpower("Rich")
 
